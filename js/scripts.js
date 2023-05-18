448f24febdf578882465932ba2649c3cbdac4c85
divitoau <divitoau@gmail.com>
2023-05-18 17:51:14 -0400

    defined pokedex variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
-alert('Hello world!');
-let favoriteFood = 'panzerotti';
-document.write(favoriteFood);
+let pokemonList = [
+    {
+        name: 'Oddish',
+        height: .5,
+        types: ['grass', 'poison']
+    },
+    {
+        name: 'Steelix',
+        height: 9.2,
+        types: ['steel', 'ground']
+    },
+    {
+        name: 'Wailord',
+        height: 14.5,
+        types: ['water']
+    },
+    {
+        name: 'Regice',
+        height: 1.8,
+        types: ['ice']
+    }
+]
