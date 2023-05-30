a041d897855828c0fa0bd52cfb9fcd2cbe17a28e
divitoau <divitoau@gmail.com>
2023-05-30 16:52:03 -0400

    wrapped array in IIFE and adjusted code to accommodate

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,40 @@
-let pokemonList = [
-    {
-        name: 'Oddish',
-        height: .5,
-        types: ['grass', 'poison']
-    },
-    {
-        name: 'Steelix',
-        height: 9.2,
-        types: ['steel', 'ground']
-    },
-    {
-        name: 'Wailord',
-        height: 14.5,
-        types: ['water']
-    },
-    {
-        name: 'Regice',
-        height: 1.8,
-        types: ['ice']
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: 'Oddish',
+            height: .5,
+            types: ['grass', 'poison']
+        },
+        {
+            name: 'Steelix',
+            height: 9.2,
+            types: ['steel', 'ground']
+        },
+        {
+            name: 'Wailord',
+            height: 14.5,
+            types: ['water']
+        },
+        {
+            name: 'Regice',
+            height: 1.8,
+            types: ['ice']
+        }
+    ]
+        function add(pokemon) {
+            pokemonList.push(pokemon);
+        }
+        function getAll() {
+            return pokemonList;
+        }
+    return {
+        add,
+        getAll
     }
-]
 
-pokemonList.forEach(function (pokemon) {
+})()
+
+pokemonRepository.getAll().forEach(function (pokemon) {
     if (pokemon.height > 10) {
         document.write(`${pokemon.name} (height: ${pokemon.height}) - Wow, that's big!<br>`)
     } else {
